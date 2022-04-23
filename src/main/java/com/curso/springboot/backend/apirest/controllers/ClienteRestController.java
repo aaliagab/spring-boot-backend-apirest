983de6c002c8326860bbccb000076cc4944e209c
Adriel Alejandro Aliaga Benavides <aaliagab85@gmail.com>
2022-04-22 20:29:07 -0400

    uso de cors con CrossOrigin
    compartiendo recursos de nuestro api rest al dominio de app angular mediante uso de cors

--- src/main/java/com/curso/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/curso/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -3,12 +3,17 @@ package com.curso.springboot.backend.apirest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.curso.springboot.backend.apirest.models.entity.Cliente;
 import com.curso.springboot.backend.apirest.models.service.IClienteService;
+
+//compartiendo recursos de nuestro api rest al dominio de app angular mediante uso de cors
+//tambien es posible restringir otros parametros
+@CrossOrigin(origins = {"http://localhost:4200"})
 
 @RestController
 @RequestMapping("/api")
