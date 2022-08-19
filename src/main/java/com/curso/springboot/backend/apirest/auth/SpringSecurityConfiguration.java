4aea7068abb63d428ba3b054659dee2a5b041d9b
Adriel Alejandro Aliaga Benavides <aaliagab85@gmail.com>
2022-08-18 21:38:14 -0400

    ResourceServerConfig
    Creando reglas de autenticacion

--- src/main/java/com/curso/springboot/backend/apirest/auth/SpringSecurityConfiguration.java
+++ src/main/java/com/curso/springboot/backend/apirest/auth/SpringSecurityConfiguration.java
@@ -3,18 +3,21 @@ package com.curso.springboot.backend.apirest.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter{
+public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Autowired 
+	@Autowired
 	private UserDetailsService usuarioService;
-	
+
 	@Bean
 	public static BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -33,6 +36,17 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter{
 		// TODO Auto-generated method stub
 		return super.authenticationManager();
 	}
-	
-	
+
+	// SE ADICIONAN TODAS LAS REGLAS DE ACCESO SEGUN LOS ROLES DE USUARIOS O URLS,
+	// ETC
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		http.authorizeRequests()
+		.anyRequest()
+		.authenticated().and()
+		.csrf().disable()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
 }
